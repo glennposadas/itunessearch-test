25aadb122106d85ffd9614e1e5439ddf16828ad2
Glenn Posadas <hello@glennvon.com>
2020-04-10 02:53:07 +0800

    add data cell template.

---
+++ iSearch/iSearch/View/Search/Cell/DataTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  DataTableViewCell.swift
+//  iSearch
+//
+//  Created by Glenn Von Posadas on 4/10/20.
+//  Copyright © 2020 CitusLabs. All rights reserved.
+//
+
+import UIKit
+
+/**
+ The `DataTableViewCell` is merely the common cell to be used in this project.
+ A collection cell was put into consideration but it would take time.
+ Collection cell to group songs and other `kind`s, much like the original Itunes Store app.
+ 
+ However though, it is being aimed that the artwork to be dynamic based on `kind`.
+ */
+class DataTableViewCell: BaseTableViewCell {
+
+    // MARK: - Properties
+    
+    // MARK: - Functions
+    // MARK: Overrides
+
+
+}
