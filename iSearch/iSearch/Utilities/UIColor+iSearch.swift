727373680837a81110b089ffd7dc4ac24f7710b2
Glenn Posadas <hello@glennvon.com>
2020-04-09 20:59:06 +0800

    add UIColor category.

---
+++ iSearch/iSearch/Utilities/UIColor+iSearch.swift
@@ -0,0 +1,24 @@
+//
+//  UIColor+iSearch.swift
+//  iSearch
+//
+//  Created by Glenn Von Posadas on 4/9/20.
+//  Copyright © 2020 CitusLabs. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+    /// Color mainly used for error internet notifier
+    static let lalaErrorRed: UIColor = UIColor(red:0.69, green:0, blue:0.13, alpha:0.87)
+    
+    /// Hex to UIColor.
+    class func colorWithRGBHex(_ hex: Int, alpha: Float = 1.0) -> UIColor {
+        let r = Float((hex >> 16) & 0xFF)
+        let g = Float((hex >> 8) & 0xFF)
+        let b = Float((hex) & 0xFF)
+        
+        return UIColor(red: CGFloat(r / 255.0), green: CGFloat(g / 255.0), blue:CGFloat(b / 255.0), alpha: CGFloat(alpha))
+    }
+}
