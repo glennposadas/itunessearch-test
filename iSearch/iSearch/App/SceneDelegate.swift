103da635031b466443454a436e3fb635611912a0
Glenn Posadas <hello@glennvon.com>
2020-04-09 17:37:37 +0800

    temp master-detail setup.

--- iSearch/iSearch/App/SceneDelegate.swift
+++ iSearch/iSearch/App/SceneDelegate.swift
@@ -12,12 +12,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let splitViewController =  UISplitViewController()
+        splitViewController.delegate = self
+        let masterVC = MasterViewController()
+        let detailVC = DetailViewController()
+        let masterNavController = UINavigationController(rootViewController: masterVC)
+        let detailNavController = UINavigationController(rootViewController: detailVC)
+        splitViewController.viewControllers = [masterNavController,detailNavController]
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
