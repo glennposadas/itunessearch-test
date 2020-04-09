388a835334e625d554c4db502c6f968c40dc2e07
Glenn Posadas <hello@glennvon.com>
2020-04-10 01:06:09 +0800

    add trust policy manager.

---
+++ iSearch/iSearch/Networking/Core/TrustPolicyManager.swift
@@ -0,0 +1,22 @@
+//
+//  TrustPolicyManager.swift
+//  iSearch
+//
+//  Created by Glenn Von Posadas on 4/10/20.
+//  Copyright © 2020 CitusLabs. All rights reserved.
+//
+
+import Alamofire
+import Foundation
+
+/**
+ The server trust policy to be used in Moya Manager.
+ */
+class TrustPolicyManager : ServerTrustPolicyManager {
+    override func serverTrustPolicy(forHost host: String) -> ServerTrustPolicy? {
+        return .disableEvaluation
+    }
+    public init() {
+        super.init(policies: [:])
+    }
+}
