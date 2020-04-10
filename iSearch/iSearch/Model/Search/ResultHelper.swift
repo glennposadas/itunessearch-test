c6d3f942d7f088257a89a3fa7a9512cedfffe0cd
Glenn Posadas <hello@glennvon.com>
2020-04-10 18:32:29 +0800

    add more getters for result helper.

--- iSearch/iSearch/Model/Search/ResultHelper.swift
+++ iSearch/iSearch/Model/Search/ResultHelper.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import Kingfisher
+import SwiftDate
 
 /**
  The helper for `Result`. Useful for extracting presentables.
@@ -26,6 +28,102 @@ extension Result {
             
             // Defaults to feature-movie and tv-episode.
             return .long
+        }
+    }
+    
+    /// Determines if we are to hide the extra label for date, if movies and books.
+    var dateLabelIsHidden: Bool {
+        self.cellType == .long
+    }
+    
+    /// Determines if we are to hide the get button
+    var getButtonIsHidden: Bool {
+        return self.kind == "feature-movie" || self.kind == "tv-episode"
+    }
+        
+    /**
+    The Get button title.
+    - Since we don't have much time, we can just make a couple of catchers for currency symbol.
+    - Ref: https://developer.apple.com/documentation/foundation/nslocale/1642814-currencysymbol
+    */
+    var currencySymbol: String {
+        get {
+            if self.currency == "USA" {
+                return "$"
+            } else if self.currency == "AUD" {
+                return "$"
+            } else if self.currency == "PHP" {
+                return "₱"
+            } else {
+                return "$"
+            }
+        }
+    }
+    
+    // MARK: - Presentables
+    
+    /// The trackname presentable
+    var titlePresentable: String {
+        get {
+            return self.trackName ?? ""
+        }
+    }
+    
+    /**
+     The subtitle presentable, usually the grayed out part.
+        - For song, it's the artist + album.
+        - For audiobooks, it's the author.
+        - For movies and tv-epidosdes, it's the genres.
+     */
+    var subtitlePresentable: String {
+        get {
+            if self.wrapperType == "audiobook" {
+                return self.artistName ?? ""
+            }
+            
+            if self.kind == "song" {
+                return "\(self.artistName ?? "") - \(self.collectionName ?? "")"
+            }
+            
+            // Defaults to feature-movie and tv-episode.
+            return self.primaryGenreName ?? ""
+        }
+    }
+    
+    var datePresentable: String {
+        get {
+            let releaseDate = self.releaseDate ?? ""
+            let format = "MMM d, yyyy"
+            let formattedDate = releaseDate.toDate(format, region: .current)
+            return formattedDate?.toString() ?? ""
+        }
+    }
+    
+    /// Get button title presentable
+    var getButtonTitlePresentable: String {
+        get {
+            switch self.cellType {
+            case .long: return "GET"
+            case .short: return self.trackPricePresentable
+            }
+        }
+    }
+    
+    /// The track price. Let's just focus on the `trackPrice` for now.
+    var trackPricePresentable: String {
+        get {
+            let trackPrice = self.trackPrice ?? 0.00
+            let trackPriceString = String(format:"%.2f", trackPrice)
+            return "\(self.currencySymbol) \(trackPriceString)"
+        }
+    }
+    
+    /// The artwork 100
+    var artworkResource: ImageResource {
+        get {
+            let artworkUrlString = self.artworkUrl100 ?? self.artworkUrl60 ?? ""
+            let url = URL(string: artworkUrlString)!
+            return ImageResource(downloadURL: url, cacheKey: url.absoluteString)
         }
     }
 }
