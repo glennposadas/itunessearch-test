cd9a84b7259c7b5e09dd21dda7d39e2e9e994297
Glenn Posadas <hello@glennvon.com>
2020-04-11 03:15:42 +0800

    add more master UITest cases.

--- iSearch/iSearchUITests/SearchMasterUITests.swift
+++ iSearch/iSearchUITests/SearchMasterUITests.swift
@@ -19,10 +19,25 @@ class SearchMasterUITests: BaseUITestCase {
         
         // Static Texts - Tabs
         XCTAssert(largeTitle.exists)
-        XCTAssert(app.tabBars.buttons["Recents"].exists)
-        XCTAssert(app.tabBars.buttons["Downloads"].exists)
-        XCTAssert(app.tabBars.buttons["Search"].exists)
-        XCTAssert(app.tabBars.buttons["Featured"].exists)
-        XCTAssert(app.tabBars.buttons["More"].exists)
+        XCTAssert(app.tabBars.buttons["Recents"].exists, "This tab must be existing")
+        XCTAssert(app.tabBars.buttons["Downloads"].exists, "This tab must be existing")
+        XCTAssert(app.tabBars.buttons["Search"].exists, "This tab must be existing")
+        XCTAssert(app.tabBars.buttons["Featured"].exists, "This tab must be existing")
+        XCTAssert(app.tabBars.buttons["More"].exists, "This tab must be existing")
+    }
+    
+    /// Test pull to refresh.
+    func testPullToRefresh() {
+        let firstCell = app.staticTexts["Star Wars: The Rise of Skywalker"]
+        let start = firstCell.coordinate(withNormalizedOffset: (CGVector(dx: 0, dy: 0)))
+        let finish = firstCell.coordinate(withNormalizedOffset: (CGVector(dx: 0, dy: 6)))
+        start.press(forDuration: 0, thenDragTo: finish)
+        XCTAssert(firstCell.exists, "First cell Star Wars: The Rise of Skywalker must be existing")
+    }
+    
+    func testShowDetail() {
+        let firstCell = app.staticTexts["Star Wars: The Rise of Skywalker"]
+        firstCell.tap()
+        XCTAssert(app.staticTexts["Star Wars: The Rise of Skywalker"].exists, "The title of the tapped test cell must be existing in the detail screen.")
     }
 }
