ef7f0af2fc264334fb6807ce08e35babd7cf4370
Glenn Posadas <hello@glennvon.com>
2020-04-11 03:28:34 +0800

    fix search detail ui test case.

--- iSearch/iSearchUITests/SearchDetailUITests.swift
+++ iSearch/iSearchUITests/SearchDetailUITests.swift
@@ -26,6 +26,11 @@ class SearchDetailUITests: BaseUITestCase {
         XCTAssert(app.staticTexts["$ 19.99 BUY"].exists, "Buy button must exist.")
         XCTAssert(app.staticTexts["$ 5.99 RENT"].exists, "Rent button must exist.")
         XCTAssert(app.staticTexts["About the Movie"].exists, "About the movie label must exist.")
-        XCTAssert(app.staticTexts["In STAR WARS: THE RISE OF SKYWALKER, the riveting conclusion of the landmark Skywalker saga, new legends will be born—and the final battle for freedom is yet to come. WARNING: SOME FLASHING-LIGHTS SCENES IN THIS FILM MAY AFFECT PHOTOSENSITIVE VIEWERS"].exists, "About the movie label value must exist.")
+        
+        let searchAboutTheMovieValue = "In STAR WARS: THE RISE OF SKYWALKER, the riveting conclusion of the landmark Skywalker saga, new legends will be born—and the final battle for freedom is yet to come. WARNING: SOME FLASHING-LIGHTS SCENES IN THIS FILM MAY AFFECT PHOTOSENSITIVE VIEWERS"
+        let predicate = NSPredicate(format: "label CONTAINS[c] %@", searchAboutTheMovieValue)
+        let elementQuery = app.staticTexts.containing(predicate)
+        
+        XCTAssert(elementQuery.count > 0, "About the movie label value must exist.")
     }
 }
