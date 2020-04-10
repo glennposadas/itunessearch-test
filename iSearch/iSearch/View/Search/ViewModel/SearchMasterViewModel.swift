68047c4f9547a1280a5a5f35efbaf21e000b1835
Glenn Posadas <hello@glennvon.com>
2020-04-10 15:06:45 +0800

    add network data handling search master viewmodel.

--- iSearch/iSearch/View/Search/ViewModel/SearchMasterViewModel.swift
+++ iSearch/iSearch/View/Search/ViewModel/SearchMasterViewModel.swift
@@ -25,6 +25,8 @@ class SearchMasterViewModel: BaseViewModel {
     private var mediaType = "movie"
     private weak var delegate: SearchMasterDelegate?
     
+    private var searchResult: SearchResult?
+    
     // MARK: - Functions
     
     /// Do searching... call `SearchService`.
@@ -32,40 +34,18 @@ class SearchMasterViewModel: BaseViewModel {
         searchProvider.request(.search(term: term, country: country, media: media)) { (result) in
             switch result {
             case let .success(response):
-                let data = response.data
-                
-                if response.statusCode == 200 {
-                    onSuccess?(jsonObj)
+                if response.statusCode == 200,
+                    let searchResult = try? JSONDecoder().decode(SearchResult.self, from: response.data) {
+                    self.searchResult = searchResult
+                    self.delegate?.reloadData()
                     return
                 }
-                
-                // Error handling is inconsistent in API side.
-                // the jsonObj contains `error` keys if it's for auth, and if for the others, it contains `code`.
-                let llfErrorCode = (jsonObj["code"].string ?? jsonObj["error"].string) ?? ""
-                
-                // Return the parsed error message from `LLFError`.
-                if LLFError(value: llfErrorCode).errorMessagePresentable != LLFError.defaultMessage {
-                    let errorMessagePresentable = LLFError(value: llfErrorCode).errorMessagePresentable
-                    onError?(errorMessagePresentable, response.statusCode, llfErrorCode)
-                    return
-                }
-                
-                // Return the message from server if there's no parsed error message from `LLFError`.
-                if let message = jsonObj["message"].string {
-                    onError?(message, response.statusCode, llfErrorCode)
-                    return
-                }
-                
-                if let errorDescription = jsonObj["error_description"].string {
-                    onError?(errorDescription, response.statusCode, llfErrorCode)
-                    return
-                }
-                
-                // Return a generic error message if none of the above conditions are true.
-                onError?(LLFError.defaultMessage, response.statusCode, llfErrorCode)
+
+                // TODO: handle error further...
+                self.showError("Data error")
                 
             case let .failure(error):
-                UIViewController.current()?.alert(title: <#T##String#>, okayButtonTitle: <#T##String#>, withBlock: <#T##UIViewController.AlertCallBack?##UIViewController.AlertCallBack?##(Bool) -> Void#>)
+                self.showError(error.localizedDescription)
             }
         }
     }
