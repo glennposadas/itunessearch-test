8e31a92c82049240564bebd1386197b68114c7eb
Glenn Posadas <hello@glennvon.com>
2020-04-10 14:27:15 +0800

    add baseviewmodel.

---
+++ iSearch/iSearch/View/Base/ViewModel/BaseViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  BaseViewModel.swift
+//  iSearch
+//
+//  Created by Glenn Von Posadas on 4/10/20.
+//  Copyright © 2020 CitusLabs. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+/// The Base Delegate of all ViewModels.
+@objc protocol BaseViewModelDelegate: class { }
+
+/**
+ The Base ViewModel of all ViewModels of the app.
+ This contains the common properties of all viewModels.
+ */
+class BaseViewModel: NSObject { }
