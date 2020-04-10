39e830e06f8e565d76afe474d7d846416235d837
Glenn Posadas <hello@glennvon.com>
2020-04-10 21:00:26 +0800

    add appdefaultskey

---
+++ iSearch/iSearch/App/AppDefaultsKey.swift
@@ -0,0 +1,13 @@
+//
+//  AppDefaultsKey.swift
+//  iSearch
+//
+//  Created by Glenn Von Posadas on 4/10/20.
+//  Copyright © 2020 CitusLabs. All rights reserved.
+//
+
+import Foundation
+
+struct AppDefaultsKey {
+    static let lastViewed
+}
