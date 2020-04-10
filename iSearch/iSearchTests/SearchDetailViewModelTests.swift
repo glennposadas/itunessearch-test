8dd2209b111811531d7c732095fc749c82f5620b
Glenn Posadas <hello@glennvon.com>
2020-04-11 04:37:45 +0800

    add complete SearchDetailViewModelTests cases. ✅😎

--- iSearch/iSearchTests/SearchDetailViewModelTests.swift
+++ iSearch/iSearchTests/SearchDetailViewModelTests.swift
@@ -7,7 +7,26 @@
 //
 
 import XCTest
+@testable import iSearch
 
 class SearchDetailViewModelTests: BaseUnitTests {
+    func testSelectedResult() {
+        XCTAssert(self.searchDetailViewModel.selectedResult != nil, "the selected result object must not be nil!")
+    }
     
+    func testPresentableBehaviorRelays() {
+        let vm = self.searchDetailViewModel!
+        
+        // No need for assert messages, see detail UI tests or result helper tests.
+        XCTAssert(vm.trackTitlePresentable.value == "Star Wars: The Rise of Skywalker")
+        XCTAssert(vm.contentRatingPresentable.value == " M ")
+        XCTAssert(vm.genreDatePresentable.value == "Action & Adventure • 2019")
+        XCTAssert(vm.buyButtonTitlePresentable.value == "$ 19.99 BUY")
+        XCTAssert(vm.rentButtonTitlePresentable.value == "$ 5.99 RENT")
+        
+        let searchAboutTheMovieValue = "In STAR WARS: THE RISE OF SKYWALKER, the riveting conclusion of the landmark Skywalker saga, new legends will be born—and the final battle for freedom is yet to come. WARNING: SOME FLASHING-LIGHTS SCENES IN THIS FILM MAY AFFECT PHOTOSENSITIVE VIEWERS"
+        XCTAssert(vm.aboutPresentable.value == searchAboutTheMovieValue)
+        
+        XCTAssert(vm.artworkResource.value != nil)
+    }
 }
