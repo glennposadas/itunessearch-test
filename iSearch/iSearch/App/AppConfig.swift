fa8be329c4e4d97ad9262d81a49646748d65da05
Glenn Posadas <hello@glennvon.com>
2020-04-10 14:46:49 +0800

    add appconfig.

---
+++ iSearch/iSearch/App/AppConfig.swift
@@ -0,0 +1,28 @@
+//
+//  AppConfig.swift
+//  iSearch
+//
+//  Created by Glenn Von Posadas on 4/10/20.
+//  Copyright © 2020 CitusLabs. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Holds the configuration of the whole app.
+ Such as the current region/country, global settings, and whatnot.
+ */
+struct AppConfig {
+    
+    // MARK: - Properties
+    
+    static var country: String {
+        get {
+            return "au"
+        }
+    }
+
+    // MARK: - Functions
+    // MARK: Overrides
+
+}
