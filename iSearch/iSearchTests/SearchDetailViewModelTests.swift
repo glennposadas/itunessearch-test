07dfd728aada0da08bdee85077342ee763d51545
Glenn Posadas <hello@glennvon.com>
2020-04-11 04:14:19 +0800

    add SearchDetailViewModelTests.

---
+++ iSearch/iSearchTests/SearchDetailViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  SearchDetailViewModelTests.swift
+//  iSearchTests
+//
+//  Created by Glenn Von Posadas on 4/11/20.
+//  Copyright © 2020 CitusLabs. All rights reserved.
+//
+
+import XCTest
+
+class SearchDetailViewModelTests: BaseUnitTests {
+    
+}
